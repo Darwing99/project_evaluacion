37da8081e5e89493925b5ea1aa83952e2a96fdaf
Darwing <darwing01000100@gmail.com>
2022-02-24 21:57:09 -0600

    v1.1.1

--- Views/modelos/read_modelo.php
+++ Views/modelos/read_modelo.php
@@ -2,6 +2,9 @@
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 include_once("../../Config/Config.php");
 include_once("../../Model/Conector.php");
 include_once("../../Controller/modelos.php");
--- Views/tipos_auto/read_tipo_auto.php
+++ Views/tipos_auto/read_tipo_auto.php
@@ -1,7 +1,9 @@
 <?php
-
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 include_once("../../Config/Config.php");
 include_once("../../Model/Conector.php");
 include_once("../../Controller/tipo_autos.php");
