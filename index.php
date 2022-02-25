0bce0eff9ddcbd20eeab4f1b760c999d1f348dec
Darwing <darwing01000100@gmail.com>
2022-02-24 21:43:50 -0600

    v1.1

--- index.php
+++ index.php
@@ -11,7 +11,59 @@
 </head>
 <body>
 
+<header>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Gestion de vehiculos</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
+    <div class="collapse navbar-collapse" id="navbarColor01">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Home
+            <span class="visually-hidden">(current)</span>
+          </a>
+        </li>
+      
+        
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-sm-2" type="text" placeholder="Search">
+        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+</header>
+<main>
+    <div class=" p-2">
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://images.unsplash.com/photo-1546637706-5be7e9636f5b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80&h=400" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <button class="btn btn-success">Mas Informacion</button>
+       
+      </div>
+    </div>
+   
+  </div>
+ 
+</div>
+
+
+    </div>
+</main>
+<footer>
+    <div class="row">
+        <div class="col pt-5 text-center">
+            <p>@Darwing Hernandez</p>
+        </div>
+
+    </div>
+</footer>
     
 </body>
 </html>
